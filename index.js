cb41ba2e5d25fe7c7da2a5d3c19f14fdef94314e
Ankit4454 <kanteliyaankit@gmail.com>
2023-12-04 13:05:18 +0530

    Fixed asset path

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 app.use(express.urlencoded({extended: true}));
 
-app.use(express.static('assets'));
+app.use(express.static(path.join(__dirname,'assets')));
 
 app.use(expressLayouts);
 app.set('layout extractStyles', true);
